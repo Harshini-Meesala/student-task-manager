0cee08526662a47bfc7e3375ced343311da128c3
Harshini Meesala <harshinihoney2005@gmail.com>
2026-07-06 21:22:20 +0530

    Updated frontend

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,16 @@
 require("dotenv").config();
 
 const express = require("express");
-const cors = require("cors");
 const connectDB = require("./config/db");
-
-console.log(process.env.MONGO_URI);
 
 const app = express();
 
-// Connect to MongoDB
 connectDB();
 
-// Middleware
-app.use(cors());
 app.use(express.json());
 
-// Test Route
-app.get("/", (req, res) => {
-    res.send("🚀 Student Task Manager Backend is Running!");
-});
-
-// Start Server
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`🚀 Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
