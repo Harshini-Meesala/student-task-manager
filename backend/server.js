ec13c6d4ca57ecda1cb5594622c0b76ab3b638e2
Harshini Meesala <harshinihoney2005@gmail.com>
2026-07-08 22:01:01 +0530

    Updated webpage frontend

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,27 @@
 require("dotenv").config();
 
+console.log("PORT =", process.env.PORT);
+console.log("MONGO_URI =", process.env.MONGO_URI);
+
 const express = require("express");
+const cors = require("cors");
 const connectDB = require("./config/db");
 
 const app = express();
 
-connectDB();
-
+// Middleware
+app.use(cors());
 app.use(express.json());
 
+// Connect MongoDB
+connectDB();
+
+// Test API
+app.get("/", (req, res) => {
+    res.send("Server is running");
+});
+
+// Port
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
